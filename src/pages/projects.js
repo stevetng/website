c23027d4af715e754d84c44213135ad5524e97f8
Your Name <stevetng@usc.edu>
2023-10-11 17:56:49 -0400

    project updates

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -18,6 +18,8 @@ handleVisitedState('higbie-link');
 handleVisitedState('twitter-link');
 handleVisitedState('usc-entre-link');
 handleVisitedState('btc-link');
+handleVisitedState('gpt-code-link');
+handleVisitedState('gpt-site-link');
 
 
 export default function Projects() {
@@ -30,7 +32,7 @@ export default function Projects() {
                 </li>
                 <li><p style={{ display: 'inline' }}><span className="font-bold">USC Entrepreneurship Guide</span> -- I wrote up a blog post detailing different parts of the USC entrepreneurial ecosystem to give beginners a place to start exploring.</p> [<a id="usc-entre-link" className="text-blue-700 focus:text-purple-900 focus:outline-none underline" href="https://medium.com/@stevetng/usc-entrepreneurship-a-beginners-guide-to-getting-involved-373a5cc832de" target="_blank">Link</a>]
                 </li>
-                <li><p style={{ display: 'inline' }}><span className="font-bold">Case Study on Twitter Fleets</span> -- This was my first foray into product management/prototyping as part of the first Product Buds Fellowship. I, along with Drithi Iyer and Sumegha Aryal, made a 27 page pitch deck on how Fleets could have been improved. </p>[<a id="twitter-link" className="text-blue-700 focus:text-purple-900 focus:outline-none underline" href="https://docs.google.com/presentation/d/1CEWb3mwOMln45hiXBhtQoC2js33JywQL/edit?usp=sharing&ouid=101262000765985839539&rtpof=true&sd=true" target="_blank">Link</a>]
+                <li><p style={{ display: 'inline' }}><span className="font-bold">Case Study on Twitter Fleets</span> -- (With Drithi Iyer and Sumegha Aryal) This was my first foray into product management/prototyping as part of the first Product Buds Fellowship. I made a 27 page pitch deck on how Fleets could have been improved. </p>[<a id="twitter-link" className="text-blue-700 focus:text-purple-900 focus:outline-none underline" href="https://docs.google.com/presentation/d/1CEWb3mwOMln45hiXBhtQoC2js33JywQL/edit?usp=sharing&ouid=101262000765985839539&rtpof=true&sd=true" target="_blank">Link</a>]
                 </li>
                 <li><p style={{ display: 'inline' }}><span className="font-bold">GBTC + BTC Quant Case Study</span> -- (Lead by Andrew Fisher) For my master's level quant class, we examined the relationship between BTC and Grayscale's Bitcoin trust due to the discount we noticed of GBTC. We used a L/S strategy to determine if there was potential arbitrage opportunities here.</p> [<a id="btc-link" className="text-blue-700 focus:text-purple-900 focus:outline-none underline" href="https://docs.google.com/document/d/1asTmgKw8J0mIl82fPoGlA0S3wTyWIBdcUi-RUitPcnw/edit?usp=sharing" target="_blank">Link</a>]
                 </li>
