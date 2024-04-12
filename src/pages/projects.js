3cbc17a0c7c6beb18fa07ad7db3affdb1116c560
Your Name <stevetng@usc.edu>
2024-04-12 17:17:31 -0400

    updating projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -41,7 +41,10 @@ export default function Projects() {
             <div id="projects_page">
                 <h1 className="block text-2xl my-3 mx-0 font-bold">Projects</h1>
                 <p className="block my-4 mx-0">My side projects include everything from live events, to random coding, to quant things, to research projects to writing. Click <a id="home-link" className="text-blue-700 focus:text-purple-900 focus:outline-none underline" href="/">here</a> to go back home.</p>
+                
                 <ul className="block list-disc m-4 mx-0 pl-10">
+                    <li><p style={{ display: 'inline' }}><span className="font-bold">News, but make it sports.</span> -- I played around with the EXA and Open AI APIs and email servers to send myself a weekly email containing the latest political news articles. I used the APIs to fetch and then summarize the articles in the tone of a sports narrator so they'd be more interesting to read.  </p>[<a id="crawler-link" className="text-blue-700 focus:text-purple-900 focus:outline-none underline" href="https://github.com/stevetng/sports_narrator" target="_blank">Link</a>]
+                              </li>
                     <li><p style={{ display: 'inline' }}><span className="font-bold">Web Crawling Visualization</span> -- A passion project inspired by how Google uses web crawlers to index trillions of URLs. I used a bunch of python libraries to build a website crawler that takes in a URL and creates a graph of URLs mentioned on the site.  </p>[<a id="crawler-link" className="text-blue-700 focus:text-purple-900 focus:outline-none underline" href="https://github.com/stevetng/web-crawler" target="_blank">Link</a>]
                           </li>
                     <li><p style={{ display: 'inline' }}><span className="font-bold">iMessage Data Analysis</span> -- Used SQL + ChatGPT to answer questions like who are the top 10 people I text? </p>[<a id="twitter-link" className="text-blue-700 focus:text-purple-900 focus:outline-none underline" href="https://twitter.com/WoahIsThatSteve/status/1742696109875900787" target="_blank">Link</a>]
