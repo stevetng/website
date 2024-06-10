535f9551b9a7f8e8e55329b4df5561d2e870ef8b
stevetng <stevetng@usc.edu>
2024-06-10 01:20:09 -0400

    adding ello site

---
+++ src/pages/ello.js
@@ -0,0 +1,8 @@
+export default function Projects() {  
+    return (
+        <>
+        <h1>ello, thanks for supporting olipop</h1>
+        <a href="http://olipop.com/">olipop link</a>
+        </>
+    )
+}
