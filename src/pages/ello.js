c835bf9ae178e3d5555afb2d784f7133af76f69d
stevetng <stevetng@usc.edu>
2024-06-10 01:34:27 -0400

    adding ello site pt3

--- src/pages/ello.js
+++ src/pages/ello.js
@@ -1,8 +1,8 @@
 export default function Ello() {  
     return (
         <>
-        <h1>ello, thanks for supporting olipop</h1>
-        <a href="http://olipop.com/">olipop link</a>
+        <h1>ello, thanks for supporting olipop and for the affiliate money</h1>
+        <a href="http://olipop.com/">olipop link(lol not really)</a>
         </>
     )
 }
