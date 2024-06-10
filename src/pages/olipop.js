6f8c0f0a942523dfe2d28b3e6730d8ab9e3d9cc4
stevetng <stevetng@usc.edu>
2024-06-10 01:46:10 -0400

    adding olipop

--- src/pages/olipop.js
+++ src/pages/olipop.js
@@ -1,8 +1,8 @@
 export default function Olipop() {  
     return (
         <>
-        <h1>ello, thanks for supporting olipop and for the affiliate money</h1>
-        <a href="http://olipop.com/">olipop link(lol not really)</a>
+        <h1>hey mari, thanks for supporting olipop and for the affiliate money</h1>
+        <a href="http://olipop.com/">olipop link</a>
         </>
     )
 }
